527937a65daf2fe5267491d354b6085bfd962a39
apoliszk <apolis@qq.com>
2015-09-06 23:25:05 +0800

    add numbers to button

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-function fuzz(x, f) {
-    return x + Math.random() * f - f / 2;
-}
+var MSG_FONT_SIZE = 14;
+var CANVAS_FONT_SIZE = 70;
 
-// estimate the movement of the arm
-// x0: start
-// x1: end
-// t: step from 0 to 1
-function handDrawMovement(x0, x1, t) {
-    return x0 + (x0 - x1) * (15 * Math.pow(t, 4) - 6 * Math.pow(t, 5) - 10 * Math.pow(t, 3));
-}
+var CANVAS_WIDTH = 440;
+var CANVAS_HEIGHT = 710;
+var CANVAS_PADDING = 5;
 
-// inspired by this paper
-// http://iwi.eldoc.ub.rug.nl/FILES/root/2008/ProcCAGVIMeraj/2008ProcCAGVIMeraj.pdf
-function handDrawLine(x0, y0, x1, y1) {
+var MAX_LINE_LENGTH_PRE_FRAME = 40;
+
+function drawLine(x0, y0, x1, y1) {
     var d = Math.sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
-
-    var steps = d / 40;
+    var steps = d / MAX_LINE_LENGTH_PRE_FRAME;
     if (steps < 1) steps = 1;
 
     var curStartX = x0;
@@ -24,23 +18,15 @@ function handDrawLine(x0, y0, x1, y1) {
     var curEndX;
     var curEndY;
 
-    // fuzzyness
-    var f = 4.0;
-    for (var i = 1; i <= steps; i++) {
-        var t1 = i / steps;
-        var t0 = t1 - 1 / steps;
+    for (var i = 1; i < steps; i++) {
+        curEndX = x0 + (x1 - x0) * i / steps;
+        curEndY = y0 + (y1 - y0) * i / steps;
 
-        curEndX = handDrawMovement(x0, x1, t1);
-        curEndY = handDrawMovement(y0, y1, t1);
-
-        addActionToDrawArr({
+        addActionToArr({
+            type: 'line',
             start: {
                 x: curStartX,
                 y: curStartY
-            },
-            control: {
-                x: fuzz(handDrawMovement(x0, x1, t0), f),
-                y: fuzz(handDrawMovement(y0, y1, t0), f)
             },
             end: {
                 x: curEndX,
@@ -51,67 +37,113 @@ function handDrawLine(x0, y0, x1, y1) {
         curStartX = curEndX;
         curStartY = curEndY;
     }
+
+    addActionToArr({
+        type: 'line',
+        start: {
+            x: curStartX,
+            y: curStartY
+        },
+        end: {
+            x: x1,
+            y: y1
+        }
+    });
 }
 
-function addActionToDrawArr(action) {
-    if (drawArr.length == 0) {
-        requestAnimationFrame(onFrame);
+function drawRect(x, y, w, h, txt) {
+    drawLine(x, y, x + w, y);
+    drawLine(x + w, y, x + w, y + h);
+    drawLine(x + w, y + h, x, y + h);
+    drawLine(x, y + h, x, y);
+
+    if (txt) {
+        var txtWidth = ctx.measureText(txt).width;
+        addActionToArr({
+            type: 'text',
+            txt: txt,
+            x: x + (w - txtWidth) / 2,
+            y: y + (h - CANVAS_FONT_SIZE) / 2 + CANVAS_FONT_SIZE * .8
+        });
     }
-    drawArr.push(action);
 }
 
-function addStringToLetterArr(str) {
-    if (letterArr.length == 0) {
-        requestAnimationFrame(onFrame);
+function addStringToActionArr(str, clear) {
+    if (clear) {
+        addActionToArr({
+            type: 'letter',
+            act: 'clear'
+        });
     }
     for (var i = 0, len = str.length; i < len; i++) {
-        letterArr.push(str.charAt(i));
+        addActionToArr({
+            type: 'letter',
+            act: 'append',
+            letter: str.charAt(i)
+        });
     }
+}
+
+function addActionToArr(action) {
+    if (actionArr.length == 0) {
+        requestAnimationFrame(onFrame);
+    }
+    actionArr.push(action);
 }
 
 function onFrame() {
-    if (letterArr.length > 0) {
-        var letter = letterArr.shift();
-        headerInfo.innerHTML += letter;
-    } else if (drawArr.length > 0) {
-        var action = drawArr.shift();
-        ctx.moveTo(action.start.x, action.start.y);
-        ctx.quadraticCurveTo(action.control.x, action.control.y, action.end.x, action.end.y);
-        ctx.stroke();
+    if (actionArr.length > 0) {
+        var action = actionArr.shift();
+
+        switch (action.type) {
+            case 'letter':
+                if (action.act == 'append')
+                    screenMsgDiv.innerHTML += action.letter;
+                else if (action.act == 'clear')
+                    screenMsgDiv.innerHTML = '';
+                break;
+            case 'line':
+                ctx.moveTo(action.start.x, action.start.y);
+                ctx.lineTo(action.end.x, action.end.y);
+                ctx.stroke();
+                break;
+            case 'text':
+                ctx.fillText(action.txt, action.x, action.y);
+                break;
+        }
     }
-    if (letterArr.length > 0 || drawArr.length > 0) requestAnimationFrame(onFrame);
+    if (actionArr.length > 0) requestAnimationFrame(onFrame);
 }
 
-function drawRect(x, y, w, h) {
-    handDrawLine(x, y, x + w, y);
-    handDrawLine(x + w, y, x + w, y + h);
-    handDrawLine(x + w, y + h, x, y + h);
-    handDrawLine(x, y + h, x, y);
-}
-
-var drawArr = [];
-var letterArr = [];
-
-var headerInfo = document.getElementById('headerInfo');
+var actionArr = [];
 var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+var canvasBound = canvas.getBoundingClientRect();
+var ctx = ctx = canvas.getContext('2d');
+ctx.font = CANVAS_FONT_SIZE + 'px Arial';
 
-addStringToLetterArr('Enter the password to get in...');
+var screenMsgDiv = document.getElementById('screenMsg');
+screenMsgDiv.style.fontSize = MSG_FONT_SIZE + 'px';
+screenMsgDiv.style.left = 45 + canvasBound.left + 'px';
+screenMsgDiv.style.top = 50 - MSG_FONT_SIZE * 2 + canvasBound.top + 'px';
 
-drawRect(5, 5, 430, 695);
+drawRect(CANVAS_PADDING, CANVAS_PADDING, CANVAS_WIDTH - CANVAS_PADDING * 2, CANVAS_HEIGHT - CANVAS_PADDING * 2);
 
 drawRect(45, 50, 350, 100);
 
-drawRect(45, 190, 100, 100);
-drawRect(170, 190, 100, 100);
-drawRect(295, 190, 100, 100);
+drawRect(45, 190, 100, 100, '9');
+drawRect(170, 190, 100, 100, '8');
+drawRect(295, 190, 100, 100, '7');
 
-drawRect(45, 315, 100, 100);
-drawRect(170, 315, 100, 100);
-drawRect(295, 315, 100, 100);
+drawRect(45, 315, 100, 100, '6');
+drawRect(170, 315, 100, 100, '5');
+drawRect(295, 315, 100, 100, '4');
 
-drawRect(45, 440, 100, 100);
-drawRect(170, 440, 100, 100);
-drawRect(295, 440, 100, 100);
+drawRect(45, 440, 100, 100, '3');
+drawRect(170, 440, 100, 100, '2');
+drawRect(295, 440, 100, 100, '1');
 
-drawRect(170, 565, 100, 100);
+drawRect(170, 565, 100, 100, '0');
+
+addStringToActionArr('Enter the password to get in ...', true);
