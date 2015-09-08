f74cc84901f7d9887b6f3c4746f085d99b4fec81
unknown <kzhang@RAN.hillstonedev.com>
2015-09-08 17:17:02 +0800

    draw kindle

--- script.js
+++ script.js
@@ -2,142 +2,113 @@ var CONSTANTS = {
     lineLenghtDrawnPerFrame: 20
 };
 
-function PasswordPad() {
+function Kindle() {
     this.actionArr = [];
 
-    this.canvas = document.getElementById('pwdPadCanvas');
+    this.canvas = document.getElementById('kindleCanvas');
     this.canvas.width = this.CANVAS_WIDTH;
     this.canvas.height = this.CANVAS_HEIGHT;
     this.canvasContext = this.canvas.getContext('2d');
-
     this.canvasBound = this.canvas.getBoundingClientRect();
-    this.msgDiv = document.getElementById('pwdPadMsgDiv');
-    this.msgDiv.style.fontSize = this.MSG_FONT_SIZE + 'px';
-    this.msgDiv.style.left = this.HORIZONTAL_PAD_PADDING + this.CANVAS_PADDING + this.canvasBound.left + 'px';
-    this.msgDiv.style.top = this.PAD_PADDING_TOP + this.CANVAS_PADDING - this.MSG_FONT_SIZE * 2 + this.canvasBound.top + 'px';
-
-    this.canvas.addEventListener('mousedown', wrapFunction(this.mouseDownHandler, this));
-    this.canvas.addEventListener('mouseup', wrapFunction(this.mouseUpHandler, this));
 }
 
-PasswordPad.prototype = {
-    MSG_FONT_SIZE: 14,
-    NUM_PAD_FONT_SIZE: 70,
-    SCREEN_FONT_SIZE: 70,
-    CANVAS_WIDTH: 440,
-    CANVAS_HEIGHT: 710,
-    CANVAS_PADDING: 5,
-    HORIZONTAL_PAD_PADDING: 40,
-    PAD_PADDING_TOP: 45,
-    SCREEN_HEIGHT: 100,
-    KEY_WIDTH: 100,
-    KEY_HEIGHT: 100,
+Kindle.prototype = {
+    LOGO_FONT_SIZE: 70,
+    SCREEN_FONT_SIZE: 14,
+    CANVAS_WIDTH: 585,
+    CANVAS_HEIGHT: 820,
+    PAD_WIDTH: 575,
+    PAD_HEIGHT: 810,
+    PAD_RADIUS: 40,
+    SCREEN_WIDTH: 460,
+    SCREEN_HEIGHT: 615,
+    KEY_DOT_RADIUS: 4,
+    KEY_LINE_HEIGHT: 160,
     addAction: addAction,
     drawLine: drawLine,
-    drawRect: drawRect
+    drawCurve: drawCurve,
+    drawRect: drawRect,
+    drawRoundRect: drawRoundRect
 };
-PasswordPad.prototype.drawKey = function(x, y, w, h, num) {
-    this.drawRect(x, y, w, h);
-    var fontSize = this.NUM_PAD_FONT_SIZE;
 
-    var keyDiv = document.createElement('div');
-    keyDiv.className = 'keyDiv';
-    keyDiv.style.width = w - 2 + 'px';
-    keyDiv.style.height = h - 2 + 'px';
-    keyDiv.style.left = this.canvasBound.left + 1 + x + 'px';
-    keyDiv.style.top = this.canvasBound.top + 1 + y + 'px';
-    keyDiv.style.fontSize = this.NUM_PAD_FONT_SIZE + 'px';
-    keyDiv.style.lineHeight = h - 2 + 'px';
-    keyDiv.innerHTML = num;
-
-    this.addAction({
-        type: 'appendDiv',
-        element: keyDiv
-    });
-};
-PasswordPad.prototype.doAction = function(action) {
+Kindle.prototype.doAction = function(action) {
+    var ctx = this.canvasContext;
     switch (action.type) {
         case 'drawLine':
-            this.canvasContext.moveTo(action.start.x, action.start.y);
-            this.canvasContext.lineTo(action.end.x, action.end.y);
-            this.canvasContext.stroke();
+            ctx.moveTo(action.start.x, action.start.y);
+            ctx.lineTo(action.end.x, action.end.y);
+            ctx.stroke();
+            break;
+        case 'drawCurve':
+            ctx.moveTo(action.start.x, action.start.y);
+            ctx.quadraticCurveTo(action.control.x, action.control.y, action.end.x, action.end.y);
+            ctx.stroke();
+            break;
+        case 'circleFill':
+            ctx.beginPath();
+            ctx.arc(action.x, action.y, action.r, 0, 2 * Math.PI);
+            ctx.fill();
             break;
         case 'appendDiv':
             document.body.appendChild(action.element);
-            break;
-        case 'msg':
-            if (action.act == 'append')
-                this.msgDiv.innerHTML += action.letter;
-            else if (action.act == 'clear')
-                this.msgDiv.innerHTML = '';
             break;
         default:
             break;
     }
 };
-PasswordPad.prototype.showStringOnMsgDiv = function(str) {
-    this.addAction({
-        type: 'msg',
-        act: 'clear'
-    });
-    for (var i = 0, len = str.length; i < len; i++) {
-        this.addAction({
-            type: 'msg',
-            act: 'append',
-            letter: str.charAt(i)
-        });
-    }
-};
-PasswordPad.prototype.drawPad = function() {
-    this.drawRect(this.CANVAS_PADDING,
-        this.CANVAS_PADDING,
-        this.CANVAS_WIDTH - this.CANVAS_PADDING * 2,
-        this.CANVAS_HEIGHT - this.CANVAS_PADDING * 2);
-};
-PasswordPad.prototype.drawScreen = function() {
-    var x = this.CANVAS_PADDING + this.HORIZONTAL_PAD_PADDING;
-    var y = this.CANVAS_PADDING + this.PAD_PADDING_TOP;
-    var w = this.CANVAS_WIDTH - (this.CANVAS_PADDING + this.HORIZONTAL_PAD_PADDING) * 2;
-    var h = this.SCREEN_HEIGHT;
-    this.drawRect(x, y, w, h);
 
-    var screenDiv = document.createElement('div');
+Kindle.prototype.drawPad = function() {
+    this.padX = (this.CANVAS_WIDTH - this.PAD_WIDTH) / 2;
+    this.padY = (this.CANVAS_HEIGHT - this.PAD_HEIGHT) / 2;
+
+    this.drawRoundRect(this.padX, this.padY, this.PAD_WIDTH, this.PAD_HEIGHT, this.PAD_RADIUS);
+};
+
+Kindle.prototype.drawScreen = function() {
+    this.screenX = (this.CANVAS_WIDTH - this.SCREEN_WIDTH) / 2;
+    this.screenY = (this.CANVAS_HEIGHT - this.SCREEN_HEIGHT) * .4;
+    this.drawRect(this.screenX, this.screenY, this.SCREEN_WIDTH, this.SCREEN_HEIGHT);
+
+    this.screenDiv = document.createElement('div');
     screenDiv.className = 'screenDiv';
-    screenDiv.style.width = w + 'px';
-    screenDiv.style.height = h + 'px';
-    screenDiv.style.left = this.canvasBound.left + x + 'px';
-    screenDiv.style.top = this.canvasBound.top + y + 'px';
+    screenDiv.style.width = this.SCREEN_WIDTH + 'px';
+    screenDiv.style.height = this.SCREEN_HEIGHT + 'px';
+    screenDiv.style.left = this.canvasBound.left + this.screenX + 'px';
+    screenDiv.style.top = this.canvasBound.top + this.screenY + 'px';
     screenDiv.style.fontSize = this.SCREEN_FONT_SIZE + 'px';
-    screenDiv.style.lineHeight = h + 'px';
 
     this.addAction({
         type: 'appendDiv',
         element: screenDiv
     });
 };
-PasswordPad.prototype.drawKeys = function() {
-    var left = this.CANVAS_PADDING + this.HORIZONTAL_PAD_PADDING;
-    var leftWidth = this.CANVAS_WIDTH - left * 2;
-    var gap = (leftWidth - this.KEY_WIDTH * 3) / 2;
-    var leftHeight = this.CANVAS_HEIGHT - this.CANVAS_PADDING * 2 - this.PAD_PADDING_TOP - this.SCREEN_HEIGHT;
-    var top = this.CANVAS_PADDING + this.PAD_PADDING_TOP + this.SCREEN_HEIGHT + (leftHeight - this.KEY_HEIGHT * 4 - gap * 3) / 2;
 
-    var x;
-    var y;
-    for (var i = 0; i < 9; i++) {
-        x = left + (i % 3) * (this.KEY_WIDTH + gap);
-        y = top + parseInt(i / 3) * (this.KEY_HEIGHT + gap);
-        this.drawKey(x, y, this.KEY_WIDTH, this.KEY_HEIGHT, 9 - i);
-    }
+Kindle.prototype.drawKeys = function() {
+    var leftDot = {
+        type: 'circleFill',
+        x: this.padX + (this.PAD_WIDTH - this.SCREEN_WIDTH) / 4,
+        y: this.padY + this.PAD_HEIGHT / 3,
+        r: this.KEY_DOT_RADIUS
+    };
 
-    x -= this.KEY_WIDTH + gap;
-    y += this.KEY_HEIGHT + gap;
-    this.drawKey(x, y, this.KEY_WIDTH, this.KEY_HEIGHT, 9 - i);
+    var rightDot = {
+        type: 'circleFill',
+        x: this.padX + this.PAD_WIDTH - (this.PAD_WIDTH - this.SCREEN_WIDTH) / 4,
+        y: this.padY + this.PAD_HEIGHT / 3,
+        r: this.KEY_DOT_RADIUS
+    };
+
+    var lineStartY = this.padY + this.PAD_HEIGHT / 2;
+    var lineEndY = this.padY + this.PAD_HEIGHT / 2 + this.KEY_LINE_HEIGHT;
+
+    this.addAction(leftDot);
+    this.drawLine(leftDot.x, lineStartY, leftDot.x, lineEndY);
+
+    this.addAction(rightDot);
+    this.drawLine(rightDot.x, lineStartY, rightDot.x, lineEndY);
 };
-PasswordPad.prototype.mouseDownHandler = function() {
 
-};
-PasswordPad.prototype.mouseUpHandler = function() {
+Kindle.prototype.drawLogo = function() {
 
 };
 
@@ -185,11 +156,41 @@ function drawLine(x0, y0, x1, y1) {
     });
 }
 
+function drawCurve(startX, startY, controlX, controlY, endX, endY) {
+    this.addAction({
+        type: 'drawCurve',
+        start: {
+            x: startX,
+            y: startY
+        },
+        control: {
+            x: controlX,
+            y: controlY
+        },
+        end: {
+            x: endX,
+            y: endY
+        }
+    });
+}
+
 function drawRect(x, y, w, h) {
     this.drawLine(x, y, x + w, y);
     this.drawLine(x + w, y, x + w, y + h);
     this.drawLine(x + w, y + h, x, y + h);
     this.drawLine(x, y + h, x, y);
+}
+
+function drawRoundRect(x, y, w, h, r) {
+    this.canvasContext.moveTo(x + r, y);
+    this.drawLine(x + r, y, x + w - r, y);
+    this.drawCurve(x + w - r, y, x + w, y, x + w, y + r);
+    this.drawLine(x + w, y + r, x + w, y + h - r);
+    this.drawCurve(x + w, y + h - r, x + w, y + h, x + w - r, y + h);
+    this.drawLine(x + w - r, y + h, x + r, y + h);
+    this.drawCurve(x + r, y + h, x, y + h, x, y + h - r);
+    this.drawLine(x, y + h - r, x, y + r);
+    this.drawCurve(x, y + r, x, y, x + r, y);
 }
 
 function addAction(action) {
@@ -200,10 +201,10 @@ function addAction(action) {
 }
 
 function onFrame() {
-    if (pwdPad.actionArr.length > 0) {
-        pwdPad.doAction(pwdPad.actionArr.shift());
+    if (kindle.actionArr.length > 0) {
+        kindle.doAction(kindle.actionArr.shift());
     }
-    if (pwdPad.actionArr.length > 0) requestAnimationFrame(onFrame);
+    if (kindle.actionArr.length > 0) requestAnimationFrame(onFrame);
 }
 
 function wrapFunction(func, scope) {
@@ -212,8 +213,8 @@ function wrapFunction(func, scope) {
     };
 }
 
-var pwdPad = new PasswordPad();
-pwdPad.drawPad();
-pwdPad.drawScreen();
-pwdPad.drawKeys();
-pwdPad.showStringOnMsgDiv('Enter the password', true);
+var kindle = new Kindle();
+kindle.drawPad();
+kindle.drawScreen();
+kindle.drawKeys();
+kindle.drawLogo();
