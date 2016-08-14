d3c0ae6b189669e77ff13c83b93d62c0aeee8ec2
apoliszk <apolis@qq.com>
2016-08-14 20:31:34 +0800

    change color and add more keyword

--- pages/Code2Html/convert.js
+++ pages/Code2Html/convert.js
@@ -41,7 +41,7 @@ function convertIndent(line) {
 
 var stringReg = /((?:\'[^\']*\')|(?:\"[^\"]*\"))/g;
 var fnNameReg = /\b(\w+)\b([ ]*\()/g;
-var keywordReg = /\b((?:new)|(?:for)|(?:while)|(?:do)|(?:if)|(?:else)|(?:return)|(?:return)|(?:try)|(?:catch)|(?:var)|(?:function))\b/g;
+var keywordReg = /\b((?:new)|(?:for)|(?:while)|(?:do)|(?:if)|(?:else)|(?:return)|(?:break)|(?:try)|(?:catch)|(?:var)|(?:function)|(?:switch)|(?:case)|(?:default))\b/g;
 function highlight(line) {
     line = line.replace(stringReg, '<span class="code-string">$1</span>');
     line = line.replace(fnNameReg, '<span class="code-fn-name">$1</span>$2');
