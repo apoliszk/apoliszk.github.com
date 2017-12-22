d82fdffe49bea252a3b756ee6d895bfb01325619
kzhang <apolis@qq.com>
2017-12-22 18:04:50 +0800

    create index pages

---
+++ make-index.js
@@ -0,0 +1,76 @@
+const EXCLUDE_DIR_NAMES = /^\./;
+const START_MARK = '<!--AUTO-->';
+const PATH = 'http://www.apolis.pub'
+let fs = require('fs');
+
+scanDirAndMakeHtml('.');
+
+function scanDirAndMakeHtml(dir) {
+    let names = fs.readdirSync(dir);
+
+    if (names.indexOf('index.html') >= 0) {
+        let content = fs.readFileSync(`${dir}/index.html`, 'utf-8');
+        if (content.trim().indexOf(START_MARK) != 0)
+            return;
+    }
+
+    let dirs = [];
+
+    names.forEach(name => {
+        if (EXCLUDE_DIR_NAMES.test(name)) return;
+
+        let stat = fs.statSync(`${dir}/${name}`);
+        if (stat.isDirectory()) {
+            dirs.push(name);
+            scanDirAndMakeHtml(`${dir}/${name}`);
+        }
+    });
+
+    fs.writeFileSync(`${dir}/index.html`, makeHtml(dirs, dir));
+}
+
+function makeHtml(subDirs, curDir) {
+    let html = `${START_MARK}
+<style>
+h1 {
+    margin: 1em calc(1em - 16px);
+}
+ul {
+    line-height: 1.5;
+    list-style-type: none;
+}
+ul li {
+    position: relative;
+}
+ul li::before {
+    content: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAd5JREFUeNqMU79rFUEQ/vbuodFEEkzAImBpkUabFP4ldpaJhZXYm/RiZWsv/hkWFglBUyTIgyAIIfgIRjHv3r39MePM7N3LcbxAFvZ2b2bn22/mm3XMjF+HL3YW7q28YSIw8mBKoBihhhgCsoORot9d3/ywg3YowMXwNde/PzGnk2vn6PitrT+/PGeNaecg4+qNY3D43vy16A5wDDd4Aqg/ngmrjl/GoN0U5V1QquHQG3q+TPDVhVwyBffcmQGJmSVfyZk7R3SngI4JKfwDJ2+05zIg8gbiereTZRHhJ5KCMOwDFLjhoBTn2g0ghagfKeIYJDPFyibJVBtTREwq60SpYvh5++PpwatHsxSm9QRLSQpEVSd7/TYJUb49TX7gztpjjEffnoVw66+Ytovs14Yp7HaKmUXeX9rKUoMoLNW3srqI5fWn8JejrVkK0QcrkFLOgS39yoKUQe292WJ1guUHG8K2o8K00oO1BTvXoW4yasclUTgZYJY9aFNfAThX5CZRmczAV52oAPoupHhWRIUUAOoyUIlYVaAa/VbLbyiZUiyFbjQFNwiZQSGl4IDy9sO5Wrty0QLKhdZPxmgGcDo8ejn+c/6eiK9poz15Kw7Dr/vN/z6W7q++091/AQYA5mZ8GYJ9K0AAAAAASUVORK5CYII=");
+    position: absolute;
+    left: -24px;
+    top: 5px;
+}
+ul.parent li::before {
+    content: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAmlJREFUeNpsU0toU0EUPfPysx/tTxuDH9SCWhUDooIbd7oRUUTMouqi2iIoCO6lceHWhegy4EJFinWjrlQUpVm0IIoFpVDEIthm0dpikpf3ZuZ6Z94nrXhhMjM3c8895977BBHB2PznK8WPtDgyWH5q77cPH8PpdXuhpQT4ifR9u5sfJb1bmw6VivahATDrxcRZ2njfoaMv+2j7mLDn93MPiNRMvGbL18L9IpF8h9/TN+EYkMffSiOXJ5+hkD+PdqcLpICWHOHc2CC+LEyA/K+cKQMnlQHJX8wqYG3MAJy88Wa4OLDvEqAEOpJd0LxHIMdHBziowSwVlF8D6QaicK01krw/JynwcKoEwZczewroTvZirlKJs5CqQ5CG8pb57FnJUA0LYCXMX5fibd+p8LWDDemcPZbzQyjvH+Ki1TlIciElA7ghwLKV4kRZstt2sANWRjYTAGzuP2hXZFpJ/GsxgGJ0ox1aoFWsDXyyxqCs26+ydmagFN/rRjymJ1898bzGzmQE0HCZpmk5A0RFIv8Pn0WYPsiu6t/Rsj6PauVTwffTSzGAGZhUG2F06hEc9ibS7OPMNp6ErYFlKavo7MkhmTqCxZ/jwzGA9Hx82H2BZSw1NTN9Gx8ycHkajU/7M+jInsDC7DiaEmo1bNl1AMr9ASFgqVu9MCTIzoGUimXVAnnaN0PdBBDCCYbEtMk6wkpQwIG0sn0PQIUF4GsTwLSIFKNqF6DVrQq+IWVrQDxAYQC/1SsYOI4pOxKZrfifiUSbDUisif7XlpGIPufXd/uvdvZm760M0no1FZcnrzUdjw7au3vu/BVgAFLXeuTxhTXVAAAAAElFTkSuQmCC");
+}
+</style>
+<h1>$path</h1>
+$parent
+<hr>
+<ul>
+    $children
+</ul>`;
+
+    if (curDir == '.') {
+        html = html.replace('$path', `${PATH}`);
+        html = html.replace('$parent', '');
+    } else {
+        html = html.replace('$path', `${PATH}/${curDir.replace(/^\.\//, '')}/`);
+        html = html.replace('$parent', '<ul class="parent"><li><a href="../">Parent Directory</a></li></ul>');
+    }
+    let children = '';
+
+    subDirs.forEach(subDir => {
+        children += `<li><a href="${subDir}/">${subDir}</a></li>`
+    });
+    html = html.replace('$children', children);
+    return html;
+}
